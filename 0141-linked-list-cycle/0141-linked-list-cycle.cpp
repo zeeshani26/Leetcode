3326cb72defa484da69abfbbdb4bf1d7edae7b5c
Zeeshan Ilahi <zeeshan02679@gmail.com>
2024-11-18 20:11:11 -0500

    Time: 11 ms (43.58%), Space: 10.9 MB (20.96%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -9,14 +9,17 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        ListNode *slow = head, *fast = head;
-        while(fast && fast->next){
-            slow = slow->next;
-            fast = fast->next->next;
-            if(slow==fast){
-                return true;
-            }
+       ListNode* slow = head;
+       ListNode* fast = head;
+       // Make sure to have fast->next!=nullptr
+       // so that the pointer never attempts to do nullptr->next
+       while(fast!=nullptr && fast->next!=nullptr){
+        fast = fast->next->next;
+        slow = slow->next;
+        if(slow==fast){
+            return true;
         }
-        return false;
+       }
+       return false;
     }
 };
