7c4e0ebb3cf4000c76a89facd222fdf9886fde11
Zeeshan Ilahi <zeeshan02679@gmail.com>
2024-11-23 20:23:08 -0500

    Time: 0 ms (100%), Space: 8.4 MB (90.72%) - LeetHub

--- 3110-score-of-a-string/3110-score-of-a-string.cpp
+++ 3110-score-of-a-string/3110-score-of-a-string.cpp
@@ -2,14 +2,14 @@ class Solution {
 public:
     int scoreOfString(string s) {
         int n = s.length();
-        if(s.length() == 1){
-            return s[0];
+        if(n <= 1){
+            return 0;
         }
         int i=0;
         int j = 1;
         int sum = 0;
         while(j<n){
-            sum = sum + abs(s[i]-s[j]);
+            sum += abs(s[i]-s[j]);
             i++;
             j++;
         }
