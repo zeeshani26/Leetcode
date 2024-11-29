0c3bc584eaabfc79f14c5dd323bf88c1ea33bca2
Zeeshan Ilahi <zeeshan02679@gmail.com>
2024-11-28 19:44:37 -0500

    Time: 0 ms (100%), Space: 9.9 MB (6.8%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.cpp
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.cpp
@@ -1,17 +1,12 @@
 class Solution {
 public:
     int numIdenticalPairs(vector<int>& nums) {
-        // Brute Force Approach O(n^2)
-        int n = nums.size();
-        int count = 0;
-        for(int i=0;i<n-1;i++){
-            int j=i+1;
-            while(j<n){
-                if(nums[i]==nums[j]){
-                    count++;
-                }
-                j++;
-            }
+        // Hashmap approach
+        unordered_map<int, int> freq;
+        int count = 0; 
+        for(int i=0;i<nums.size();i++){
+            count += freq[nums[i]];
+            freq[nums[i]]++;
         }
         return count;
     }
