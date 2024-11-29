6d3bf90ff2b71fb527958eaa54cd29fe01069b9a
Zeeshan Ilahi <zeeshan02679@gmail.com>
2024-11-29 18:07:13 -0500

    Time: 0 ms (100%), Space: 22.7 MB (6.56%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -4,7 +4,7 @@ public:
         int a = 1;
         int n = nums.size();
         for(int b=0;b<n;b++){
-            if(nums[b] > nums[a-1]){
+            if(nums[b] != nums[a-1]){
                 nums[a] = nums[b];
                 a++; 
             }
