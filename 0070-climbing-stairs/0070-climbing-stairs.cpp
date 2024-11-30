d1fc6d98fb4c0bac11f3d01b266e07b552d41e9d
Zeeshan Ilahi <zeeshan02679@gmail.com>
2024-11-30 12:46:34 -0500

    Time: 0 ms (100%), Space: 8.7 MB (5.15%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -2,23 +2,22 @@ class Solution {
 public:
     int climbStairs(int n) {
         if(n==0){
+            return 0;
+        }
+        if(n==1){
             return 1;
         }
-        else if(n==1){
-            return 1;
-        }
-        else if(n==2){
+        if(n==2){
             return 2;
         }
 
-        vector<int> steps = {0,1,2};
-        steps.resize(n+1);
+        vector<int> ans = {0,1,2};
+        ans.resize(n+1);
 
         for(int i=3;i<=n;i++){
-            steps[i] = steps[i-1] + steps[i-2];
+            ans[i] = ans[i-2] + ans[i-1];
         }
-
-        return steps[n];
+        return ans[n]; 
     }
 
 };
