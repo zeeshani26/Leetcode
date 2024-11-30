5fb0e15cdf0d33eadc0cfe543eed1bdbe6a5d78f
Zeeshan Ilahi <zeeshan02679@gmail.com>
2024-11-29 22:50:30 -0500

    Time: 0 ms (100%), Space: 28.3 MB (7.73%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-
-    int candidate = 0;
-    int count = 0;
-
-    for (int i = 0; i < nums.size(); i++) {
-        if (count == 0) {
+    // majority with verification, moore voting algo
+    int count = 1;
+    int candidate = nums[0];
+    for(int i = 1; i < nums.size(); i++){
+        if(nums[i] == candidate){
+            count++;
+        }
+        else{
+            count--;
+        }
+        if(count == 0){
             candidate = nums[i];
             count = 1;
-        } else if (nums[i] == candidate) {
-            count++;
-        } else {
-            count--;
         }
     }
     return candidate;
