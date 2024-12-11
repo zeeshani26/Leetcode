488dca53919b85672e7f788828bb08ed3383eab9
Zeeshan Ilahi <zeeshan02679@gmail.com>
2024-12-11 02:40:04 -0500

    Time: 4 ms (63.69%), Space: 13.4 MB (13.53%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.cpp
+++ 1470-shuffle-the-array/1470-shuffle-the-array.cpp
@@ -1,18 +1,12 @@
 class Solution {
 public:
     vector<int> shuffle(vector<int>& nums, int n) {
-        vector<int> ans(2*n,0);
-        // two pointers
-        int p1 = 0;
-        int p2 = n;
-        int i=0;
-        while(p2 != 2*n){
-            ans[i] = nums[p1];
-            i++;
-            p1++;
-            ans[i] = nums[p2];
-            i++;
-            p2++;
+        // More readible two pointer with pushback
+        vector<int> ans; 
+        int p1 = 0, p2 = n; // Two pointers
+        while (p2 < 2 * n) { 
+            ans.push_back(nums[p1++]); 
+            ans.push_back(nums[p2++]); 
         }
         return ans;
     }
