6cd85a8cb30f0dfaf5f7086be1acd46692699597
Zeeshan Ilahi <zeeshan02679@gmail.com>
2024-12-11 20:13:20 -0500

    Time: 15 ms (7.27%), Space: 12.5 MB (18.78%) - LeetHub

---
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums) {
+        int ans = 0;
+        int tmp = 0;
+        for(int i=0; i<nums.size(); i++){
+            tmp +=  nums[i];
+            nums[i] = tmp;
+            
+            cout<<tmp<<"\n";
+        }
+        return nums;
+    }
+};
