253ef484cae9f964a82769bb6aceabb336053a54
Zeeshan Ilahi <zeeshan02679@gmail.com>
2024-12-14 02:35:17 -0500

    Time: 33 ms (29.91%), Space: 80.2 MB (89.39%) - LeetHub

--- 2410-maximum-matching-of-players-with-trainers/2410-maximum-matching-of-players-with-trainers.cpp
+++ 2410-maximum-matching-of-players-with-trainers/2410-maximum-matching-of-players-with-trainers.cpp
@@ -1,24 +1,22 @@
 class Solution {
 public:
     int matchPlayersAndTrainers(vector<int>& players, vector<int>& trainers) {
-       // two pointer force
-       sort(players.begin(), players.end()); 
-       sort(trainers.begin(), trainers.end());
-       int p = 0;
-       int t = 0;
-       int ans = 0;
-       while(p < players.size() && t<trainers.size()){
-        if(trainers[t] < players[p]){
-            t++;
-        }
-        else if(players[p] <= trainers[t]){
-            ans++;
-            t++;
-            p++;
-        }
-
-       } 
-    return ans;
+       // two pointer final
+        sort(players.begin(), players.end()); 
+        sort(trainers.begin(), trainers.end());
+        int p = 0;
+        int t = 0;
+        int ans = 0;
+        while (p < players.size() && t < trainers.size()) {
+            if (trainers[t] < players[p]) {
+                t++;
+            } else {
+                ans++;
+                t++;
+                p++;
+            }
+        } 
+        return ans;
  
     }
 };
