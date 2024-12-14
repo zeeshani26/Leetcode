ff4ee45ca74c999e20abd7af1698f803ffd309e2
Zeeshan Ilahi <zeeshan02679@gmail.com>
2024-12-14 02:52:38 -0500

    Time: 27 ms (80.96%), Space: 80.2 MB (73.42%) - LeetHub

--- 2410-maximum-matching-of-players-with-trainers/2410-maximum-matching-of-players-with-trainers.cpp
+++ 2410-maximum-matching-of-players-with-trainers/2410-maximum-matching-of-players-with-trainers.cpp
@@ -4,20 +4,16 @@ public:
        // two pointer final
         sort(players.begin(), players.end()); 
         sort(trainers.begin(), trainers.end());
-        int p = 0;
-        int t = 0;
-        int ans = 0;
+        int p = 0, t = 0, ans = 0;
+
         int nump = players.size();
         int numt = trainers.size();
-        while (p < nump && t < numt) {
-            if (players[p] <= trainers[t]) {
-                t++;
-                p++;
-                ans++;
-            } else {
-                t++;
-            }
-        } 
+
+       while (p < nump && t < numt) {
+            ans += (players[p] <= trainers[t]);
+            p += (players[p] <= trainers[t]);
+            t++; // Trainer always increments logically
+        }
         return ans;
  
     }
