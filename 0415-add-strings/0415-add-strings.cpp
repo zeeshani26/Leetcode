4aa20bf38befc5d6418532bfd613a18bee030d56
Zeeshan Ilahi <zeeshan02679@gmail.com>
2024-12-31 02:03:38 -0500

    Time: 0 ms (100%), Space: 8.9 MB (68.63%) - LeetHub

--- 0415-add-strings/0415-add-strings.cpp
+++ 0415-add-strings/0415-add-strings.cpp
@@ -1,10 +1,56 @@
 class Solution {
 public:
+    int charToInt(char c) {
+        return c - '0';
+    }
     string addStrings(string num1, string num2) {
-        int a = stoi(num1);
-        int b = stoi(num2);
-        int numres = a+b;
-        string strres = to_string(numres);
-        return strres;
+        string ans = "";
+        int i = num1.size()-1;
+        int j = num2.size()-1;
+        int carry = 0;
+        int sum;
+        while(i >= 0 && j >= 0){
+            sum = (num1[i] - '0') + (num2[j] - '0')+ carry;
+            if(sum > 9){
+                carry = sum/10; // calculate carry first 
+                sum = sum % 10;
+            }
+            else{
+                carry = 0;
+            }
+            ans +=  sum + '0';
+            i--;
+            j--;
+        }
+
+        while(i>=0){
+            sum = (num1[i] - '0') + carry;
+            if(sum>9){
+                carry = sum/10;
+                sum = sum % 10;
+            }
+            else{
+                carry = 0;
+            }
+            ans += sum + '0';
+            i--;
+        }
+        while(j>=0){
+            sum = (num2[j] - '0') + carry;
+            if(sum>9){
+                carry = sum/10;
+                sum = sum % 10;
+            }
+            else{
+                carry = 0;
+            }
+            ans += sum + '0';
+            j--;
+        }
+        if(carry>0){
+            ans += carry + '0';
+        }
+        reverse(ans.begin(), ans.end());
+        return ans;
     }
 };
