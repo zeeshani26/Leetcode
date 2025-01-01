a568799ffb828f13735c7dfbb36d5bd5ca914e69
Zeeshan Ilahi <zeeshan02679@gmail.com>
2024-12-31 21:40:58 -0500

    Time: 4 ms (31.51%), Space: 10.4 MB (32.82%) - LeetHub

--- 0415-add-strings/0415-add-strings.cpp
+++ 0415-add-strings/0415-add-strings.cpp
@@ -1,55 +1,41 @@
 class Solution {
 public:
     int charToInt(char c) {
-        return c - '0';
+        return c - '0';  // string conversion method in c and c++
     }
     string addStrings(string num1, string num2) {
         string ans = "";
         int i = num1.size()-1;
         int j = num2.size()-1;
         int carry = 0;
-        int sum;
-        while(i >= 0 && j >= 0){
-            sum = (num1[i] - '0') + (num2[j] - '0')+ carry;
-            if(sum > 9){
-                carry = sum/10; // calculate carry first 
-                sum = sum % 10;
+       while(i>=0 || j>=0 || carry){
+            //declared an int var sum and assigned 0 to it
+            long sum = 0;
+
+            // convert the char to int and add it to sum variable
+            if(i>=0){
+                sum+=num1[i]-'0';
+                i--;
             }
-            else{
-                carry = 0;
+
+            // similarily convert the char to int and it to sum var
+            if(j>=0){
+                sum+=num2[j]-'0';
+                j--;
             }
-            ans +=  sum + '0';
-            i--;
-            j--;
+
+            // add carry to the sum
+            sum+=carry;
+
+            // carry is always the leftmost digit of a number
+            carry=sum/10;
+
+            /* concatenate the last digit of the sum after converting 
+             it into string using to_string function */ 
+            ans+=to_string(sum%10);
         }
 
-        while(i>=0){
-            sum = (num1[i] - '0') + carry;
-            if(sum>9){
-                carry = sum/10;
-                sum = sum % 10;
-            }
-            else{
-                carry = 0;
-            }
-            ans += sum + '0';
-            i--;
-        }
-        while(j>=0){
-            sum = (num2[j] - '0') + carry;
-            if(sum>9){
-                carry = sum/10;
-                sum = sum % 10;
-            }
-            else{
-                carry = 0;
-            }
-            ans += sum + '0';
-            j--;
-        }
-        if(carry>0){
-            ans += carry + '0';
-        }
+        // Bceause we were going fromn left o right while calculating and adding
         reverse(ans.begin(), ans.end());
         return ans;
     }
