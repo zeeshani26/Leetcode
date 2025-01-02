17f9055561fe8699d78e9a342b51c30d01bab8f8
Zeeshan Ilahi <zeeshan02679@gmail.com>
2025-01-01 21:40:09 -0500

    Time: 0 ms (100%), Space: 8 MB (16.5%) - LeetHub

--- 2413-smallest-even-multiple/2413-smallest-even-multiple.cpp
+++ 2413-smallest-even-multiple/2413-smallest-even-multiple.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     int smallestEvenMultiple(int n) {
-      for(int i = 2; i <= 150;i += 2){
+      for(int i = 2; i <= n;i += 2){
         if(i%n==0 && i%2==0){
             return i;
         }
       }
-      return n*2;  
+      return n*2;  // when answer is not found smaller than n, multiply n by 2
     }
 };
