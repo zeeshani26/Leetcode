2b174bbe53bc3a2db5128b48ae1e614ef3887888
Zeeshan Ilahi <zeeshan02679@gmail.com>
2025-01-10 23:50:12 -0500

    Time: 3 ms (44.13%), Space: 10.1 MB (69.16%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -7,25 +7,24 @@ public:
 
         while (i >= 0) {
             if (s[i] == ' ') {
-                if (!word.empty()) {
+                if (word.empty() == false) { // to ensure no zsubsequent space
                     // If we have collected a word, append it to the answer
                     ans += word + " ";
-                    word = ""; // Reset word to collect the next one
+                    word = ""; 
                 }
             } else {
-                // Add characters to the word
                 word = s[i] + word;
             }
             i--;
         }
 
         // Add the last word to the answer (if it exists)
-        if (!word.empty()) {
+        if (word.empty() == false) {
             ans += word;
         }
 
         // Remove trailing space (if any)
-        if (!ans.empty() && ans.back() == ' ') {
+        if (ans.empty() == false && ans.back() == ' ') {
             ans.pop_back();
         }
 
