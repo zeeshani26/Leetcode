37f0c67efd2330b2fea55cc1564b295273e9d1b5
Zeeshan Ilahi <zeeshan02679@gmail.com>
2025-02-08 16:21:20 -0500

    Time: 2 ms (61.07%), Space: 19.7 MB (15.94%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -3,31 +3,28 @@ public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
         /* 1. Create a hashmap : element(key) : frequency(value)
            2. Create buckets where index is frequency and inside buckets are arrays of values eg: [[],[3],[],[1,2]*/
-
-        unordered_map<int,int> freq;
         
-        for(const int el : nums){
+        unordered_map<int, int> freq;
+        for (const int el : nums) {
             freq[el]++;
-        } 
-        //create bucket
+        }
+
+        // Step 2: Create buckets where index represents frequency
         vector<vector<int>> bucket(nums.size() + 1);
-        // add value to bucket
-        for(const auto& pair : freq){
+        for (const auto& pair : freq) {
             bucket[pair.second].push_back(pair.first);
         }
-        vector <int> ans;
-        // push answer to final array
-        for(int i=bucket.size()-1; i>=0 && k>0;i--){
-            // loop inside to get all value from the same bucket
-            if(k!=0){
-                for (const int el : bucket[i]) {
-                    ans.push_back(el);
-                    k--;
-                    if (k == 0) break;
-                }
+
+        // Step 3: Extract the top k frequent elements
+        vector<int> ans;
+        for (int i = bucket.size() - 1; i >= 0 && k > 0; i--) {
+            for (const int el : bucket[i]) {
+                ans.push_back(el);
+                k--;
+                if (k == 0) break;
             }
         }
+
         return ans;
-        
     }
 };
