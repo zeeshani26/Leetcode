5122d88c7f9f5af16a3f65934e897419a7ad9706
Zeeshan Ilahi <zeeshan02679@gmail.com>
2025-02-08 16:57:44 -0500

    Time: 4 ms (37.64%), Space: 20 MB (6.41%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -2,29 +2,29 @@ class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
         /* 1. Create a hashmap : element(key) : frequency(value)
-           2. Create buckets where index is frequency and inside buckets are arrays of values eg: [[],[3],[],[1,2]*/
-        
-        unordered_map<int, int> freq;
-        for (const int el : nums) {
-            freq[el]++;
-        }
-
-        // Step 2: Create buckets where index represents frequency
-        vector<vector<int>> bucket(nums.size() + 1);
-        for (const auto& pair : freq) {
-            bucket[pair.second].push_back(pair.first);
-        }
-
-        // Step 3: Extract the top k frequent elements
-        vector<int> ans;
-        for (int i = bucket.size() - 1; i >= 0 && k > 0; i--) {
-            for (const int el : bucket[i]) {
-                ans.push_back(el);
-                k--;
-                if (k == 0) break;
+           2. Create buckets where index is frequency and inside buckets are    arrays of values eg: [[],[3],[],[1,2]*/
+            if(k==nums.size()){
+                return nums;
             }
-        }
+            unordered_map<int,int> freq;
 
-        return ans;
+            for(const int el:nums){
+                freq[el]++;
+            }
+
+            vector<vector<int>> bucket(nums.size()+1);
+
+            for(const auto& pair : freq){
+                bucket[pair.second].push_back(pair.first);
+            }
+            vector<int> ans;
+            for(int i=bucket.size()-1; i>=0 && k>0; i--){
+                    for(int j=0;j<bucket[i].size() && k>0;j++){
+                        // cout<<bucket[i][j]<<" | ";
+                        ans.push_back(bucket[i][j]);
+                        k--;
+                    }
+           }
+           return ans;
     }
 };
