765267ac37313534af35c49c40739f60f774a444
Zeeshan Ilahi <zeeshan02679@gmail.com>
2025-02-13 23:17:39 -0600

    Time: 89 ms (36.62%), Space: 87.7 MB (28.54%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -3,24 +3,28 @@ public:
     int longestConsecutive(vector<int>& nums) {
         if (nums.empty()) return 0;
 
-        // Create a set
+        /* Create a set. Set longest to 1
+        2. In loop, check if the elements prev (e-1) doesnt exist in the set
+        3. Set currentLength as 1 and look for next (e+1) in the set use count
+        */
+        unordered_set<int> set(nums.begin(),nums.end());
 
-        unordered_set<int> numSet(nums.begin(),nums.end());
-        int longest  = 1;
-        for(int s: numSet){
-
-            if(!numSet.count(s-1)){
-                int currentLength = 1;
-                int currentNum = s+1;
-                while(numSet.count(currentNum)){
+        int longest = 1;
+        int currentLength;
+        for(int s:set){
+            
+            if(!set.count(s-1)){
+                currentLength = 1;
+                int nextElement = s+1;
+                while(set.count(nextElement)){
                     currentLength++;
-                    currentNum++;
+                    nextElement++;
                 }
-                longest = max(currentLength, longest);
+            longest = max(currentLength,longest);
+
             }
 
         }
-
-      return longest;
+        return longest;
     }
 };
