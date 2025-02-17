0e4f9bb71696bd599a4c7661bd6838d65d4df4ad
Zeeshan Ilahi <zeeshan02679@gmail.com>
2025-02-17 16:42:46 -0600

    Time: 2 ms (41.77%), Space: 10.3 MB (28.19%) - LeetHub

--- 0415-add-strings/0415-add-strings.cpp
+++ 0415-add-strings/0415-add-strings.cpp
@@ -1,56 +1,50 @@
 class Solution {
 public:
-    int charToInt(char c) {
-        return c - '0';
-    }
     string addStrings(string num1, string num2) {
-        string ans = "";
+        // accessing lastdigit of the num1 string
         int i = num1.size()-1;
+
+        // accessing lastdigit of the num2 string 
         int j = num2.size()-1;
+
+        // declared a integer variable carry and assigned it 0
         int carry = 0;
-        int sum;
-        while(i >= 0 && j >= 0){
-            sum = (num1[i] - '0') + (num2[j] - '0')+ carry;
-            if(sum > 9){
-                carry = sum/10; // calculate carry first 
-                sum = sum % 10;
+
+        // declared an empty string
+        string ans = "";
+
+        // traversing the num1 and num2 string till the leftmost digit
+        // and till carry > 0
+        while(i>=0 || j>=0 || carry){
+            //declared an int var sum and assigned 0 to it
+            long sum = 0;
+
+            // convert the char to int and add it to sum variable
+            if(i>=0){
+                sum+=num1[i]-'0';
+                i--;
             }
-            else{
-                carry = 0;
+
+            // similarily convert the char to int and it to sum var
+            if(j>=0){
+                sum+=num2[j]-'0';
+                j--;
             }
-            ans +=  sum + '0';
-            i--;
-            j--;
+
+            // add carry to the sum
+            sum+=carry;
+
+            // carry is always the leftmost digit of a number
+            carry=sum/10;
+
+            /* concatenate the last digit of the sum after converting 
+             it into string using to_string function */ 
+            ans+=to_string(sum%10);
         }
 
-        while(i>=0){
-            sum = (num1[i] - '0') + carry;
-            if(sum>9){
-                carry = sum/10;
-                sum = sum % 10;
-            }
-            else{
-                carry = 0;
-            }
-            ans += sum + '0';
-            i--;
-        }
-        while(j>=0){
-            sum = (num2[j] - '0') + carry;
-            if(sum>9){
-                carry = sum/10;
-                sum = sum % 10;
-            }
-            else{
-                carry = 0;
-            }
-            ans += sum + '0';
-            j--;
-        }
-        if(carry>0){
-            ans += carry + '0';
-        }
-        reverse(ans.begin(), ans.end());
+        /* as the required sum is obtained in the reverse manner , 
+         reverse the sum to obtain the actual answer */
+        reverse(ans.begin(),ans.end());
         return ans;
     }
 };
