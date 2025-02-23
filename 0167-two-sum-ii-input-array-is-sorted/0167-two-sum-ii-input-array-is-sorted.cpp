e9036ee6cb188f8c93dcedd160cc1a8b0e1b571f
Zeeshan Ilahi <zeeshan02679@gmail.com>
2025-02-23 00:47:57 -0500

    Time: 0 ms (100%), Space: 19.6 MB (32.44%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
@@ -7,9 +7,7 @@ public:
         int j = n-1;
         while(i < n && j>0){
             if(numbers[i]+numbers[j] == target){
-                ans.push_back(i+1);
-                ans.push_back(j+1);
-                return ans;
+                return {i+1,j+1};
             }
             else if(numbers[i]+numbers[j] > target){
                 j--;
