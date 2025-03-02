06d5b96e4a9bccbfe83521ced7eb7a716b6f5185
Zeeshan Ilahi <zeeshan02679@gmail.com>
2025-03-02 17:27:45 -0500

    Time: 0 ms (100%), Space: 14.8 MB (65.12%) - LeetHub

--- 2570-merge-two-2d-arrays-by-summing-values/2570-merge-two-2d-arrays-by-summing-values.cpp
+++ 2570-merge-two-2d-arrays-by-summing-values/2570-merge-two-2d-arrays-by-summing-values.cpp
@@ -1,31 +1,36 @@
+#include <vector>
+using namespace std;
+
 class Solution {
 public:
     vector<vector<int>> mergeArrays(vector<vector<int>>& nums1,
                                     vector<vector<int>>& nums2) {
         vector<vector<int>> ans;
-        int i = 0;
-        int j = 0;
+        int i = 0, j = 0;
+
         while (i < nums1.size() && j < nums2.size()) {
-            if (nums1[i][0] == nums2[j][0]) {
-                int sum = nums1[i][1] + nums2[j][1];
-                vector<int> add = {nums1[i][0], sum};
-                ans.push_back(add);
-            } else {
-                if (nums1[i][0] <= nums2[j][0]) {
-                    ans.push_back(nums1[i]);
-                    ans.push_back(nums2[j]);
-                } else {
-                    ans.push_back(nums2[j]);
-                    ans.push_back(nums1[i]);
-                }
+            if (nums1[i][0] == nums2[j][0]) {  
+                // Merge values if IDs match
+                ans.push_back({nums1[i][0], nums1[i][1] + nums2[j][1]});
+                i++;
+                j++; // Move both pointers
+            } else if (nums1[i][0] < nums2[j][0]) {  
+                // Add smaller ID and move its pointer
+                ans.push_back(nums1[i]);
+                i++;
+            } else {  
+                ans.push_back(nums2[j]);
+                j++;
             }
-            i++;
-            j++;
         }
+
+        // Add remaining elements from nums1
         while (i < nums1.size()) {
             ans.push_back(nums1[i]);
             i++;
         }
+
+        // Add remaining elements from nums2
         while (j < nums2.size()) {
             ans.push_back(nums2[j]);
             j++;
