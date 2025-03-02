36be7bd5b99f765b3c7fc16fb834ff4290d643e0
Zeeshan Ilahi <zeeshan02679@gmail.com>
2025-03-02 17:28:04 -0500

    Time: 2 ms (39.66%), Space: 14.8 MB (44.7%) - LeetHub

--- 2570-merge-two-2d-arrays-by-summing-values/2570-merge-two-2d-arrays-by-summing-values.cpp
+++ 2570-merge-two-2d-arrays-by-summing-values/2570-merge-two-2d-arrays-by-summing-values.cpp
@@ -1,36 +1,32 @@
-#include <vector>
-using namespace std;
-
 class Solution {
 public:
     vector<vector<int>> mergeArrays(vector<vector<int>>& nums1,
                                     vector<vector<int>>& nums2) {
         vector<vector<int>> ans;
-        int i = 0, j = 0;
-
+        int i = 0;
+        int j = 0;
         while (i < nums1.size() && j < nums2.size()) {
-            if (nums1[i][0] == nums2[j][0]) {  
-                // Merge values if IDs match
-                ans.push_back({nums1[i][0], nums1[i][1] + nums2[j][1]});
+            if (nums1[i][0] == nums2[j][0]) {
+                int sum = nums1[i][1] + nums2[j][1];
+                vector<int> add = {nums1[i][0], sum};
+                ans.push_back(add);
                 i++;
-                j++; // Move both pointers
-            } else if (nums1[i][0] < nums2[j][0]) {  
-                // Add smaller ID and move its pointer
-                ans.push_back(nums1[i]);
-                i++;
-            } else {  
-                ans.push_back(nums2[j]);
-                j++;
+            j++;
+            } else {
+                if (nums1[i][0] <= nums2[j][0]) {
+                    ans.push_back(nums1[i]);
+                    i++;
+                } else {
+                    ans.push_back(nums2[j]);
+                    j++;
+                }
             }
+            
         }
-
-        // Add remaining elements from nums1
         while (i < nums1.size()) {
             ans.push_back(nums1[i]);
             i++;
         }
-
-        // Add remaining elements from nums2
         while (j < nums2.size()) {
             ans.push_back(nums2[j]);
             j++;
