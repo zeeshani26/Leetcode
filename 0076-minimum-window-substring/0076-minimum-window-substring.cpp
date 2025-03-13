644c091a0c6495ab621c3529bc9675f889cbceff
Zeeshan Ilahi <zeeshan02679@gmail.com>
2025-03-13 15:07:45 -0400

    Time: 15 ms (37.45%), Space: 11.5 MB (63.07%) - LeetHub

---
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -0,0 +1,61 @@
+class Solution {
+public:
+    string minWindow(string s, string t) {
+        if (s.size() < t.size()) {
+            return "";
+        }
+
+        // Create hashset with values of t
+
+        unordered_map<char, int> freqMap;
+        int charFound = 0;
+
+        for (char i : t) {
+            freqMap[i]++;
+        }
+
+        // loop through s
+        int l = 0;
+        int r = 0;
+        int min = INT_MAX;
+        int startIndex = 0;
+        unordered_map<char, int> windowMap;
+        int anssize = freqMap.size(); // check number of unique characters only
+        while (r < s.size()) {
+            // Increment the char frequency in window map
+            windowMap[s[r]] += 1;
+
+            if (freqMap.count(s[r]) &&
+                freqMap[s[r]] ==
+                    windowMap[s[r]]) { // also checks for duplicates
+                charFound++;
+            }
+
+            while (charFound == anssize) {
+                int length = r - l + 1;
+                if (length < min) {
+                    min = length;
+                    startIndex = l;
+                }
+                windowMap[s[l]]--;
+                if (freqMap.count(s[l]) && windowMap[s[l]] < freqMap[s[l]]) {
+                    charFound--;
+                }
+                l++;
+            }
+            r++;
+        }
+
+        cout << startIndex << " , " << min;
+        if (min == INT_MAX) {
+            return "";
+        }
+        string ans = "";
+        int i = startIndex;
+        while (i < startIndex + min) {
+            ans += s[i];
+            i++;
+        }
+        return ans;
+    }
+};
