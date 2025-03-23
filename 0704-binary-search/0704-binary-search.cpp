386f4b3f29cf6d6706f17e80450d965d59524162
Zeeshan Ilahi <zeeshan02679@gmail.com>
2025-03-23 19:25:25 -0400

    Time: 0 ms (100%), Space: 31.2 MB (80.43%) - LeetHub

--- 0704-binary-search/0704-binary-search.cpp
+++ 0704-binary-search/0704-binary-search.cpp
@@ -1,21 +1,10 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        if (nums.size() == 1) {
-            if (nums[0] == target) {
-                return 0;
-            } else {
-                return -1;
-            }
-        }
         int left = 0;
         int right = nums.size() - 1;
-        cout<<right<<endl;
         while (left <= right) {
-            int mid = floor((left + right) / 2);
-            cout << "Position: " << mid << ", Value: " << nums[mid] << endl;
-            cout<<"Left: "<<left<<endl;
-            cout<<"Right: "<<right<<endl;
+            int mid = (left + right) / 2;
             if (nums[mid] == target) {
                 return mid;
             } else if (nums[mid] > target) {
@@ -24,7 +13,7 @@ public:
                 left = mid + 1;
             }
         }
-   
+
         return -1;
     }
 };
